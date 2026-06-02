730f20959660cb08521faf0dd3b5ce02c87b76e2
Alex-Au <AlexXianZhenYuAu@gmail.com>
2026-06-02 10:15:15 -0400

    Add search criteria for search by food
    - Food code is an exact search, while food group and food names are
    search by substring

--- app/backend.js
+++ app/backend.js
@@ -1,12 +1,23 @@
-import { SearchOpts } from "./constants.js";
-import { Translation } from "./tools.js";
+import { SearchOpts, SearchAtts, DataCols } from "./constants.js";
+import { Translation, TableTools } from "./tools.js";
 
 
 export class Model {
     constructor() {
         this.searchOpt = SearchOpts.SearchByFood;
+        this.searchInputs = this.initSearchInputs();
 
-        this.foodNameTable;
+        this.foodTable;
+    }
+
+    initSearchInputs() {
+        return {
+            [SearchOpts.SearchByFood]: {
+                [SearchAtts.FoodName]: "",
+                [SearchAtts.FoodGroup]: "",
+                [SearchAtts.FoodCode]: ""
+            }
+        };
     }
 
     // loadCSV(file): Loads the table and its columns from a CSV file
@@ -16,16 +27,64 @@ export class Model {
         return {data, columns};
     }
 
+    loadFoodTable(foodNameTable, foodGroupTable) {
+        this.foodTable = TableTools.dataLeftJoinById(foodNameTable, foodGroupTable, DataCols.FoodGroupCode, DataCols.FoodGroupCode);
+        const foodTableData = this.foodTable.data;
+        
+        const foodTableIndices = {};
+        this.foodTable.indices = foodTableIndices;
+
+        // Food code is a unique index
+        const foodCodeIndex = {};
+        foodTableIndices.foodCode = foodCodeIndex;
+        const foodTableLen = foodTableData.length;
+
+        for (let i = 0; i < foodTableLen; ++i) {
+            const row = foodTableData[i];
+            const indexVal = row[DataCols.FoodCode]
+            if (foodCodeIndex[indexVal] !== undefined) {
+                throw new TypeError("Food Code is not unique!");
+            }
+
+            foodCodeIndex[indexVal] = i;
+        }
+    }
+
     // load(): Initial load of all the required data
     async load() {
-        await Promise.all([this.loadCSV(`data/Food_Name.csv`)])
-            .then(([foodNameTable]) => {
-                this.foodNameTable = foodNameTable; 
+        await Promise.all([this.loadCSV(`data/Food_Name.csv`), this.loadCSV(`data/CNF_Food_Group.csv`)])
+            .then(([foodNameTable, foodGroupTable]) => {
+                this.loadFoodTable(foodNameTable, foodGroupTable);
+        });
+    }
+
+    filterFoodSearchTable(foodName, foodGroup, foodCode) {
+        let result = this.foodTable.data;
+
+        if (foodCode != "") {
+            const rowInd = this.foodTable.indices.foodCode[foodCode];
+            if (rowInd === undefined) return {};
+            result = [this.foodTable.data[rowInd]];
+        }
+
+        if (foodName == "" && foodGroup == "") return result;
+        
+        return result.filter((row) => {
+            if (foodName != "" && !row[Translation.getDataCol(DataCols.FoodDescription)].includes(foodName)) {
+                return false;
+            }
+
+            if (foodGroup != "" && !row[Translation.getDataCol(DataCols.FoodGroupDescription)].includes(foodGroup)) {
+                return false;
+            }
+
+            return true;
         });
     }
 
     // getFoodSearchTableData(): Retrieves the data for the searched foods 
     getFoodSearchTableData() {
-        return this.foodNameTable.data;
+        const inputs = this.searchInputs[SearchOpts.SearchByFood];
+        return this.filterFoodSearchTable(inputs[SearchAtts.FoodName], inputs[SearchAtts.FoodGroup], inputs[SearchAtts.FoodCode]);
     }
 }
